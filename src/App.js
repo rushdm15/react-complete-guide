f0bf784b9c24f49d2b6fa67bd7c4811840219256
Doug Rush <71516868+rushdm15@users.noreply.github.com>
2021-06-12 18:27:28 +0200

    Alternative to JSX code
    Must import React to Card.js, ExpenseDate.js, ExpenseItem.js, Expenses.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import ExpenseDate from "./components/ExpenseDate";
+import React from "react";
 import Expenses from "./components/Expenses";
 
 function App() {
@@ -23,6 +23,14 @@ function App() {
       date: new Date(2021, 5, 12),
     },
   ];
+
+  // Alternative to JSX code.
+  // return React.createElement(
+  //   "div",
+  //   {},
+  //   React.createElement("h2", {}, "Let's get started"),
+  //   React.createElement(Expenses, { items: expenses })
+  // );
 
   return (
     <div>
